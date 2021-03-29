93ed538506d7d8b50ca7f150193ebd8b0617f66b
Brendan Asselstine <mail.asselstine@gmail.com>
2021-03-29 16:34:08 -0700

    Fixed generate.sh script

--- generate.sh
+++ generate.sh
@@ -6,9 +6,7 @@ echo "Cloning repo..."
 git clone git@github.com:pooltogether/documentation.git gitbook
 cd gitbook
 git checkout v3.3.0
-cp ../networks/ethereum.md networks/ethereum.md
-cp ../networks/xdai.md networks/xdai.md
-cp ../networks/matic.md networks/matic.md
+cp -rf ../networks/* networks/
 git add networks
 git commit -m "Updated networks directory"
 echo "Pushing changes..."
